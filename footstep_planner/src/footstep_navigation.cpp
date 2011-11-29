a838b65a5d79e1019f675b2671ada8854377b4b4
Armin Hornung <hornunga@informatik.uni-freiburg.de>
2011-11-29 08:53:26 +0000

    missing src file added

---
+++ footstep_planner/src/footstep_navigation.cpp
@@ -0,0 +1,36 @@
+// SVN $HeadURL$
+// SVN $Id$
+
+/*
+ * A footstep planner for humanoid robots
+ *
+ * Copyright 2010-2011 Johannes Garimort, Armin Hornung, University of Freiburg
+ * http://www.ros.org/wiki/footstep_planner
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <footstep_planner/FootstepNavigation.h>
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "footstep_navigation");
+
+  footstep_planner::FootstepNavigation nav;
+
+  ros::spin();
+
+  return 0;
+}

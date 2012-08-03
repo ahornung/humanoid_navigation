d0b57d7268825b5b53c66e322058e9ca410a3ef4
Johannes Garimort <garimort.johannes@gmail.com>
2012-08-03 12:45:04 +0000

    added missing new class State

---
+++ footstep_planner/include/footstep_planner/State.h
@@ -0,0 +1,77 @@
+// SVN $HeadURL: https://alufr-ros-pkg.googlecode.com/svn/trunk/humanoid_stacks/humanoid_navigation/footstep_planner/include/footstep_planner/State2.h $
+// SVN $Id: State2.h 2793 2012-06-07 10:37:53Z Garimort.Johannes $
+
+/*
+ * A footstep planner for humanoid robots
+ *
+ * Copyright 2010-2011 Johannes Garimort, Armin Hornung, University of Freiburg
+ * http://www.ros.org/wiki/footstep_planner
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FOOTSTEP_PLANNER_STATE_H_
+#define FOOTSTEP_PLANNER_STATE_H_
+
+#include <footstep_planner/helper.h>
+
+
+namespace footstep_planner
+{
+    /**
+     * @brief A class representing the robot's pose (i.e. position and
+     * orientation) in the (continuous) world view. More precisely a state
+     * points to the robot's supporting leg.
+     */
+    class State
+    {
+    public:
+        State();
+        State(double x, double y, double theta, Leg leg);
+        ~State();
+
+        void setX(double x) { ivX = x; };
+        void setY(double y) { ivY = y; };
+        void setTheta(double theta) { ivTheta = theta; };
+        void setLeg(Leg leg) { ivLeg = leg; };
+
+        double getX() const { return ivX; };
+        double getY() const { return ivY; };
+        double getTheta() const { return ivTheta; };
+        Leg getLeg() const { return ivLeg; };
+
+    private:
+        /**
+         * @brief Compare two states on equality of x, y, theta, leg upon
+         * a certain degree of float precision.
+         */
+        bool operator ==(const State& s2) const;
+
+        /**
+         * @brief Inequality operator for two states (negates the equality
+         * operator).
+         */
+        bool operator !=(const State& s2) const;
+
+        /// The robot's pose in x direction.
+        double ivX;
+        /// The robot's pose in y direction.
+        double ivY;
+        /// The robot's orientation.
+        double ivTheta;
+        /// The robot's supporting leg.
+        Leg ivLeg;
+    };
+}
+#endif /* FOOTSTEP_PLANNER_STATE_H_ */
---
+++ footstep_planner/src/State.cpp
@@ -0,0 +1,64 @@
+// SVN $HeadURL: https://alufr-ros-pkg.googlecode.com/svn/trunk/humanoid_stacks/humanoid_navigation/footstep_planner/src/State2.cpp $
+// SVN $Id: State2.cpp 2852 2012-06-26 14:07:35Z Garimort.Johannes $
+
+/*
+ * A footstep planner for humanoid robots
+ *
+ * Copyright 2010-2011 Johannes Garimort, Armin Hornung, University of Freiburg
+ * http://www.ros.org/wiki/footstep_planner
+ *
+ * D* Lite (Koenig et al. 2002) partly based on the implementation
+ * by J. Neufeld (http://code.google.com/p/dstarlite/)
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <footstep_planner/State.h>
+
+
+namespace footstep_planner
+{
+    State::State()
+        : ivX(0.0), ivY(0.0), ivTheta(0.0), ivLeg(NOLEG)
+    {}
+
+
+    State::State(double x, double y, double theta, Leg leg)
+        : ivX(x), ivY(y), ivTheta(theta), ivLeg(leg)
+    {}
+
+
+    State::~State()
+    {}
+
+
+    bool
+    State::operator ==(const State& s2)
+    const
+    {
+        return (fabs(ivX - s2.getX()) <= FLOAT_CMP_THR &&
+                fabs(ivY - s2.getY()) <= FLOAT_CMP_THR &&
+                fabs(angles::shortest_angular_distance(
+                    ivTheta, s2.getTheta())) <= FLOAT_CMP_THR &&
+                ivLeg == s2.getLeg());
+    }
+
+
+    bool
+    State::operator !=(const State& s2)
+    const
+    {
+        return not (*this == s2);
+    }
+} // end of namespace
